fb6435f863cc191c29c78cd4f1b815a5937bc26d
Bautista <bautax1234@gmail.com>
2022-10-14 12:09:02 -0300

    Added old files

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+function add(num1,num2){
+    return num1 + num2;
+}
+
+function substract(num1,num2){
+    return num1 - num2;
+}
+
+function multiply(num1,num2){
+    return num1 * num2;
+}
+
+function divide(num1,num2){
+    return num1 / num2;
+}
+
+
+
+
+//Next section
+
+const numberSelections = document.querySelector('[data-selection-numer]')
+
+
