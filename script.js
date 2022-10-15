4960b9d73970d89d4d456b9bde9b3258a9f104aa
Bautista <bautax1234@gmail.com>
2022-10-15 10:59:47 -0300

    Display of calculator and Event listeners for each button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
-
-
+const numDisplay = document.querySelector('[data-display]');
+let actualNumber = 0;
 
 function add(num1,num2){
     return num1 + num2;
@@ -19,11 +19,39 @@ function divide(num1,num2){
     return num1 / num2;
 }
 
+function operate(num1,num2,operator){
+    console.log(num1, num2)
+}
 
 
 
-//Next section
+//OperationButtons will have the nodelist of the operation buttons
+const operationButtons = document.querySelectorAll('[data-selection-operation]')
+console.log(operationButtons)
 
-const numberSelections = document.querySelector('[data-selection-numer]')
+//NumberButtons will have the nodelist of numberButtons
+const numberButtons = document.querySelectorAll('[data-selection]')
+console.log(numberButtons)
 
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', e => {
+        const selectedNumber = numberButton.dataset.selection //Stores number selected
+        console.log(selectedNumber)
+        actualNumber += selectedNumber
+        numDisplay.innerText = actualNumber //Displays actual number
+    }) 
+})
 
+operationButtons.forEach(operationButton => {
+    operationButton.addEventListener('click', e => {
+        const selectedOperation = operationButton.dataset.selectionOperation //Stores operation selected
+        console.log(selectedOperation)
+        num1 = parseInt(actualNumber)
+        operator = selectedOperation
+        actualNumber = '0'
+        if(selectedOperation == '=') operate(num1,parseInt(actualNumber),operator)
+        
+        
+        
+    })
+})
